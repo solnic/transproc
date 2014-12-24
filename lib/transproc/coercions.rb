a3d8ecf5b5164b77b39676e12e63b30d7ba8314e
Piotr Solnica <piotr.solnica@gmail.com>
2014-12-24 20:01:51 +0100

    No need to be worried about Hash const lookup

--- lib/transproc/coercions.rb
+++ lib/transproc/coercions.rb
@@ -4,7 +4,7 @@ module Transproc
   TRUE_VALUES = [true, 1, '1', 'on', 't', 'true', 'y', 'yes'].freeze
   FALSE_VALUES = [false, 0, '0', 'off', 'f', 'false', 'n', 'no'].freeze
 
-  BOOLEAN_MAP = ::Hash[
+  BOOLEAN_MAP = Hash[
     TRUE_VALUES.product([true]) + FALSE_VALUES.product([false])
   ].freeze
 
