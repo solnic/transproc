654a5bbc1c3b0eeda6ce0a9fc136aad2d843f0e8
Piotr Solnica <piotr.solnica@gmail.com>
2015-04-10 12:45:24 +0200

    Use module_function in Transproc module

--- lib/transproc.rb
+++ lib/transproc.rb
@@ -3,23 +3,25 @@ require 'transproc/function'
 require 'transproc/composer'
 
 module Transproc
-  def self.register(*args, &block)
+  module_function
+
+  def register(*args, &block)
     name, fn = *args
     functions[name] = fn || block
   end
 
-  def self.register_from(mod)
+  def register_from(mod)
     (mod.public_methods - Module.public_methods).each do |meth|
       Transproc.register(meth, mod.method(meth))
     end
   end
 
-  def self.functions
-    @_functions ||= {}
+  def [](name)
+    functions.fetch(name)
   end
 
-  def self.[](name)
-    functions.fetch(name)
+  def functions
+    @_functions ||= {}
   end
 end
 
