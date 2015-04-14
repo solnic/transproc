d0ff52394526b101845556404fdc0a6ffa248a0d
Piotr Solnica <piotr.solnica@gmail.com>
2015-04-14 11:00:03 +0200

    Add Yard docs to Function and rename Composed=>Composite

--- lib/transproc/function.rb
+++ lib/transproc/function.rb
@@ -1,51 +1,104 @@
 module Transproc
+  # Transformation proc wrapper allowing composition of multiple procs into
+  # a data-transformation pipeline.
+  #
+  # This is used by Transproc to wrap registered methods.
+  #
+  # @api private
   class Function
-    attr_reader :fn, :args
+    # Wrapped proc or another composite function
+    #
+    # @return [Proc,Composed]
+    #
+    # @api private
+    attr_reader :fn
 
+    # Additional arguments that will be passed to the wrapped proc
+    #
+    # @return [Array]
+    #
+    # @api private
+    attr_reader :args
+
+    # @api private
     def initialize(fn, options = {})
       @fn = fn
       @args = options.fetch(:args) { [] }
     end
 
-    def name
-      fn.name
-    end
-
+    # Call the wrapped proc
+    #
+    # @param [Object] value The input value
+    #
+    # @alias []
+    #
+    # @api public
     def call(value)
       fn[value, *args]
     end
     alias_method :[], :call
 
+    # Compose this function with another function or a proc
+    #
+    # @param [Proc,Function]
+    #
+    # @return [Composite]
+    #
+    # @alias :>>
+    #
+    # @api public
     def compose(other)
-      Composed.new(fn, args: args, right: other)
+      Composite.new(fn, args: args, right: other)
     end
     alias_method :+, :compose
     alias_method :>>, :compose
 
-    class Composed < Function
+    # @api public
+    def name
+      fn.name
+    end
+
+    # Composition of two functions
+    #
+    # @api private
+    class Composite < Function
       alias_method :left, :fn
 
+      # @return [Proc]
+      #
+      # @api private
       attr_reader :right
 
+      # @api private
       def initialize(fn, options = {})
         super
         @right = options.fetch(:right)
       end
 
-      def name
-        [super, right.name]
-      end
-
+      # Call right side with the result from the left side
+      #
+      # @param [Object] value The input value
+      #
+      # @return [Object]
+      #
+      # @api public
       def call(value)
         right[left[value, *args]]
       end
       alias_method :[], :call
 
+      # @see Function#compose
+      #
+      # @api public
       def compose(other)
-        Composed.new(self, right: other)
+        Composite.new(self, right: other)
       end
       alias_method :+, :compose
       alias_method :>>, :compose
+
+      def name
+        [super, right.name]
+      end
     end
   end
 end
