4afdcdd1e1cac97c14ee82f71a7364880ead8f4c
Andrew Kozin <andrew.kozin@gmail.com>
2015-07-06 22:11:50 +0300

    Refactor registry specs
    Excluded dependency on concrete implementation of the `import` method.

--- spec/unit/registry_spec.rb
+++ spec/unit/registry_spec.rb
@@ -14,15 +14,16 @@ describe Transproc::Registry do
     end
   end
   let(:bar) { Transproc::Test::Bar = Module.new { extend Transproc::Registry } }
+  let(:baz) { Transproc::Test::Baz = Module.new { extend Transproc::Registry } }
 
   describe '.[]' do
-    subject { foo[fn, 'baz'] }
+    subject(:transproc) { foo[fn, 'baz'] }
 
     context 'from a method' do
       let(:fn) { :prefix }
 
       it 'builds a function from a method' do
-        expect(subject['qux']).to eql 'baz_qux'
+        expect(transproc['qux']).to eql 'baz_qux'
       end
     end
 
@@ -30,75 +31,66 @@ describe Transproc::Registry do
       let(:fn) { -> value, prefix { [prefix, '_', value].join } }
 
       it 'builds a function from a method' do
-        expect(subject['qux']).to eql 'baz_qux'
+        expect(transproc['qux']).to eql 'baz_qux'
       end
     end
   end
 
   describe '.t' do
-    subject { foo.t(:prefix, 'baz') }
+    subject(:transproc) { foo.t(:prefix, 'baz') }
 
     it 'is an alias for .[]' do
-      expect(subject['qux']).to eql 'baz_qux'
+      expect(transproc['qux']).to eql 'baz_qux'
     end
   end
 
   describe '.import' do
     context 'a module' do
-      subject { bar.import foo }
+      subject(:import) { bar.import foo }
 
-      it 'imports its methods' do
-        expect { subject }
-          .to change { bar.respond_to? :prefix }
-          .from(false)
-          .to(true)
+      it 'registers all its methods' do
+        import
         expect(bar[:prefix, 'baz']['qux']).to eql 'baz_qux'
       end
 
-      it { is_expected.to eq bar }
-    end
-
-    context 'a module with the same method' do
-      subject { bar.import foo }
-      before  { bar.singleton_class.send(:define_method, :prefix) { |*| nil } }
-
-      it 'redefines method' do
-        expect { subject }
-          .to change { bar[:prefix, 'baz']['qux'] }.from(nil).to('baz_qux')
+      it 'returns itself' do
+        expect(import).to eq bar
       end
     end
 
     context 'a method' do
-      subject { bar.import :prefix, from: foo }
+      before { bar.import :prefix, from: foo }
 
-      it 'imports the method with the same name' do
-        expect { subject }
-          .to change { bar.respond_to? :prefix }
-          .from(false)
-          .to(true)
+      it 'registers a transproc' do
         expect(bar[:prefix, 'bar']['baz']).to eql 'bar_baz'
       end
     end
 
-    context 'a renamed method' do
-      subject { bar.import :prefix, from: foo, as: :affix }
+    context 'an imported method' do
+      before do
+        bar.import :prefix, from: foo, as: :affix
+        baz.import :affix, from: bar
+      end
 
-      it 'imports the method with the new name' do
-        expect { subject }
-          .to change { bar.respond_to? :affix }
-          .from(false)
-          .to(true)
+      it 'registers a transproc' do
+        expect(baz[:affix, 'bar']['baz']).to eql 'bar_baz'
+      end
+    end
+
+    context 'a renamed method' do
+      before { bar.import :prefix, from: foo, as: :affix }
+
+      it 'registers a transproc under the new name' do
         expect(bar[:affix, 'bar']['baz']).to eql 'bar_baz'
       end
     end
   end
 
   describe '.uses' do
+    before { bar.uses foo }
+
     it 'is an alias for .import' do
-      expect { bar.import foo }
-        .to change { bar.respond_to? :prefix }
-        .from(false)
-        .to(true)
+      expect(bar[:prefix, 'baz']['qux']).to eql 'baz_qux'
     end
   end
 end
