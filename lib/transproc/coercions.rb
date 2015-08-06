01d95418617b5dc36352871c638300505f8340ee
Caleb Wright <awright@rentpath.com>
2015-08-06 14:45:00 -0400

    `to_boolean` coerces `nil` into `false`

--- lib/transproc/coercions.rb
+++ lib/transproc/coercions.rb
@@ -11,7 +11,7 @@ module Transproc
     extend Registry
 
     TRUE_VALUES = [true, 1, '1', 'on', 't', 'true', 'y', 'yes'].freeze
-    FALSE_VALUES = [false, 0, '0', 'off', 'f', 'false', 'n', 'no'].freeze
+    FALSE_VALUES = [false, 0, '0', 'off', 'f', 'false', 'n', 'no', nil].freeze
 
     BOOLEAN_MAP = Hash[
       TRUE_VALUES.product([true]) + FALSE_VALUES.product([false])
